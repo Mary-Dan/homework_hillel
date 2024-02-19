de7e8a60a37e62aa81db29e18debff0e3a82985a
Mary-Dan <152028992+Mary-Dan@users.noreply.github.com>
2024-02-19 18:39:47 +0200

    Add files via upload

---
+++ new_project/courses_app/templates/course_app/courses_page.html.py
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Courses Page</title>
+</head>
+<body>
+    <h2>Courses Page</h2>
+    <p>This is the Courses page</p>
+</body>
+</html>
--- new_project/members_app/views.py
+++ new_project/members_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .my_form import UserInputForm, SessionForm
+
 
 def input_page(request):
     """
@@ -12,28 +13,29 @@ def input_page(request):
     if request.method == 'POST':
         form = UserInputForm(request.POST)
         if form.is_valid():
-            user_input = form.cleaned_data['user_input']
-            return render(request, 'members_app/display_input.html', {'user_input': user_input})
+            user_input = form.cleaned_data.get('user_input')
+            if user_input is not None:
+                return render(request, 'members_app/display_input.html', {'user_input': user_input})
     else:
         form = UserInputForm()
     return render(request, 'members_app/input_page.html', {'form': form})
 
+
 def display_input(request):
     """
-    Представлення для сторінки відображення користувацького вводу.
+    Представлення для відображення введеного користувачем тексту.
 
     Parameters:
     request (HttpRequest): Об'єкт запиту, який містить дані від клієнта.
 
     """
-    if request.method == 'POST':
-        form = UserInputForm(request.POST)
-        if form.is_valid():
-            user_input = form.cleaned_data['user_input']
-            return render(request, 'members_app/display_input.html', {'user_input': user_input})
-    else:
-        form = UserInputForm()
-    return render(request, 'members_app/input_page.html', {'form': form})
+
+    user_input = request.session.get('user_input', None)
+    if user_input is None:
+        return redirect('input_page')
+
+    return render(request, 'members_app/display_input.html', {'user_input': user_input})
+
 
 def session_page(request):
     """
@@ -46,8 +48,11 @@ def session_page(request):
     if request.method == 'POST':
         form = SessionForm(request.POST)
         if form.is_valid():
-            value = form.cleaned_data['value']
-            request.session['value'] = value
+            value = form.cleaned_data.get('value')
+            if value is not None:
+                request.session['value'] = value
     else:
         form = SessionForm()
-    return render(request, 'members_app/session_page.html', {'form': form})
+
+    session_value = request.session.get('value', None)
+    return render(request, 'members_app/session_page.html', {'form': form, 'session_value': session_value})
